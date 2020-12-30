3a0deac038b64409bee55ce2bfac706727e1b9cb
park1997 <iambyunghyun97@gmail.com>
2020-12-30 16:59:05 +0900

    2020-12-30

---
+++ test(5-1)_20201230/test(5-1)_20201230/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result As String
+
+        If TextBox2.Text >= 80 Then
+            result = "합격입니다."
+        Else
+            result = "불합격입니다."
+        End If
+
+        MessageBox.Show(TextBox1.Text & "님의 점수는" & TextBox2.Text & "점 입니다. " & result)
+
+
+    End Sub
+End Class
---
+++ test(5-3)_20201230/test(5-3)_20201230/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim result As String
+
+        If TextBox2.Text >= 95 Then
+            result = "A+"
+        ElseIf TextBox2.Text >= 90 Then
+            result = "A"
+        ElseIf TextBox2.Text >= 85 Then
+            result = "B+"
+        ElseIf TextBox2.Text >= 80 Then
+            result = "B"
+
+        Else
+            result = "F"
+        End If
+
+        MessageBox.Show(TextBox1.Text & "님의 점수는 " & TextBox2.Text & "입니다." & vbCrLf & "평점은 " & result & "입니다.")
+
+    End Sub
+End Class
